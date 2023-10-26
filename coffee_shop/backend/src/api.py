4adeaad095ad8d26e83262ca91e979946653d27e
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2023-10-26 21:45:09 +0530

    chane all done TODO to DONE

--- coffee_shop/backend/src/api.py
+++ coffee_shop/backend/src/api.py
@@ -12,7 +12,7 @@ setup_db(app)
 CORS(app)
 
 '''
-@TODO uncomment the following line to initialize the datbase
+@DONE: uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
@@ -22,7 +22,7 @@ CORS(app)
 
 # ROUTES
 '''
-@TODO implement endpoint
+@DONE: implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
@@ -40,7 +40,7 @@ def get_drinks():
         }), 200
 
 '''
-@TODO implement endpoint
+@DONE: implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
@@ -59,7 +59,7 @@ def get_drinks_detail(payload):
         }), 200
 
 '''
-@TODO implement endpoint
+@DONE: implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -95,7 +95,7 @@ def post_drink(payload):
             abort(422)
 
 '''
-@TODO implement endpoint
+@DONE: implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -140,7 +140,7 @@ def patch_drink(payload, drink_id):
             abort(422)
 
 '''
-@TODO implement endpoint
+@DONE: implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -174,7 +174,7 @@ Example error handling for unprocessable entity
 
 
 '''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
+@DONE: implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
                     "success": False,
@@ -200,7 +200,7 @@ def unauthorized(error):
     }), 401
 
 '''
-@TODO implement error handler for 404
+@DONE: implement error handler for 404
     error handler should conform to general task above
 '''
 @app.errorhandler(404)
@@ -237,7 +237,7 @@ def internal_server_error(error):
 
 
 '''
-@TODO implement error handler for AuthError
+@DONE: implement error handler for AuthError
     error handler should conform to general task above
 '''
 @app.errorhandler(AuthError)
--- coffee_shop/backend/src/auth/auth.py
+++ coffee_shop/backend/src/auth/auth.py
@@ -23,7 +23,7 @@ class AuthError(Exception):
 ## Auth Header
 
 '''
-@TODO implement get_token_auth_header() method
+@DONE: implement get_token_auth_header() method
     it should attempt to get the header from the request
         it should raise an AuthError if no header is present
     it should attempt to split bearer and the token
@@ -60,7 +60,7 @@ def get_token_auth_header():
     token = headers_parts[1]
     return token
 '''
-@TODO implement check_permissions(permission, payload) method
+@DONE: implement check_permissions(permission, payload) method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
         payload: decoded jwt payload
@@ -85,7 +85,7 @@ def check_permissions(permission, payload):
 
     return True
 '''
-@TODO implement verify_decode_jwt(token) method
+@DONE: implement verify_decode_jwt(token) method
     @INPUTS
         token: a json web token (string)
 
@@ -151,7 +151,7 @@ def verify_decode_jwt(token):
             }, 400)
 
 '''
-@TODO implement @requires_auth(permission) decorator method
+@DONE: implement @requires_auth(permission) decorator method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
 
