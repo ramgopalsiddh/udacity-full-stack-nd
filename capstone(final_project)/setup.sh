7ceafb0fd1403d843d8c837b61a2dfeb7fdec217
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2023-12-07 16:34:11 +0530

    update auth0 and database in setup script

--- capstone(final_project)/setup.sh
+++ capstone(final_project)/setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+# auth setup
+export AUTH0_DOMAIN="ramgopalsiddh.us.auth0.com"
+export ALGORITHMS="RS256"
+export API_AUDIENCE="capstone"
+
+#export DATABASE_URL="postgresql://ram@localhost:5432/postgres"
+export DATABASE_URL="postgres:///capstone"
+export FLASK_APP=app
 export EXCITED="true"
+export FLASK_ENVIRONMENT=debug
+
+# message show after run script successfully
 echo "setup.sh script executed successfully!"
