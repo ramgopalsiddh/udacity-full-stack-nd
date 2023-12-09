941f1e8f932994838ba7bbb14f8200d812143a7f
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2023-12-09 18:00:21 +0530

    add database in database URL

--- capstone(final_project)/setup.sh
+++ capstone(final_project)/setup.sh
@@ -4,8 +4,8 @@ export AUTH0_DOMAIN="ramgopalsiddh.us.auth0.com"
 export ALGORITHMS="RS256"
 export API_AUDIENCE="capstone"
 
-#export DATABASE_URL="postgresql://ram@localhost:5432/postgres"
-export DATABASE_URL="postgres:///capstone"
+export DATABASE_URL="postgresql://ram@localhost:5432/capstone"
+# export DATABASE_URL="postgres:///capstone"
 export FLASK_APP=app
 export EXCITED="true"
 export FLASK_ENVIRONMENT=debug
