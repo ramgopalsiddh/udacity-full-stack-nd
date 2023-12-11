4b945f2161b405c971d73c14f86d12e2c53b576d
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2023-12-11 22:15:40 +0530

    add test code

--- capstone/test_app.py
+++ capstone/test_app.py
@@ -1,1 +1,16 @@
+import unittest
+try:
+    from app import app
+except ImportError as e:
+    print("Some modules are missing: {}".format(e))
 
+class FlaskTestCase(unittest.TestCase):
+    
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+if __name__ == "__main__":
+    unittest.main()
